bc3d94147f77080877e08c04c28cf4387d105332
evgeny <eaegorov@gmail.com>
2024-07-24 10:52:29 +0500

    ДЗ 24.2

--- users/serilazers.py
+++ users/serilazers.py
@@ -14,4 +14,4 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ("data_payment", "summ_payment", "payment_method", "pay_list")
+        fields = ("__all__")
