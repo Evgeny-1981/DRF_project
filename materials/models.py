88d4c065c8d1930c3fb63d2905f4c63df690cb52
Evgeny Egorov <EAEgorov42@gmail.com>
2024-07-27 19:45:35 +0500

    25.1 - добавил в модель поле owner

--- materials/models.py
+++ materials/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 
+from config.settings import AUTH_USER_MODEL
+
 NULLABLE = {"blank": True, "null": True}
 
 
 class Course(models.Model):
     name = models.CharField(max_length=120, verbose_name="наименование курса")
     description = models.TextField(verbose_name="описание курса", **NULLABLE)
-    image = models.ImageField(
-        upload_to="media/course_image/", verbose_name="изображение", **NULLABLE
-    )
+    image = models.ImageField(upload_to="media/course_image/", verbose_name="изображение", **NULLABLE)
+    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="автор курса", **NULLABLE)
 
     def __str__(self):
         return f"{self.name}"
@@ -23,19 +24,11 @@ class Course(models.Model):
 class Lesson(models.Model):
     name = models.CharField(max_length=120, verbose_name="наименование урока")
     description = models.TextField(verbose_name="описание урока", **NULLABLE)
-    image = models.ImageField(
-        upload_to="media/lesson_image/", verbose_name="изображение", **NULLABLE
-    )
-    link_video = models.FileField(
-        upload_to="media/lesson_video", verbose_name="видео", **NULLABLE
-    )
-    name_course = models.ForeignKey(
-        Course,
-        related_name="lesson",
-        verbose_name="название курса",
-        on_delete=models.CASCADE,
-        **NULLABLE,
-    )
+    image = models.ImageField(upload_to="media/lesson_image/", verbose_name="изображение", **NULLABLE)
+    link_video = models.FileField(upload_to="media/lesson_video", verbose_name="видео", **NULLABLE)
+    name_course = models.ForeignKey(Course, related_name="lesson", verbose_name="название курса",
+                                    on_delete=models.CASCADE, **NULLABLE, )
+    owner = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name="автор урока", **NULLABLE)
 
     def __str__(self):
         return f"{self.name}"
